fixed the velocity scaling issue of the dmp

mp_pytorch/demo/data_for_demo.py
@@ -26,7 +26,7 @@ def get_mp_utils(mp_type: str, learn_tau=False, learn_delay=False):
     scale_factor = 1
     if "dmp" in config.mp_type:
         num_param += config.num_dof
-        scale_factor = 100
+        scale_factor = 20
 
     # assume we have 3 trajectories in a batch
     num_traj = 3
@@ -57,7 +57,10 @@ def get_mp_utils(mp_type: str, learn_tau=False, learn_delay=False):
 
     bc_time = times[:, 0]
     bc_pos = 5 * torch.ones([num_traj, config.num_dof])
-    bc_vel = -5 * torch.ones([num_traj, config.num_dof])
+    if config.learn_delay:
+        bc_vel = torch.zeros_like(bc_pos)
+    else:
+        bc_vel = -5 * torch.ones([num_traj, config.num_dof])
 
     demos = torch.zeros([*times.shape, config.num_dof])
     for i in range(config.num_dof):

mp_pytorch/dmp.py
@@ -126,7 +126,7 @@ class DMP(MPInterface):
         assert torch.all(torch.abs(self.bc_time - self.times[..., 0]) < 1e-8), \
             "The first time step's value should be same to bc_time."
         pos[..., 0, :] = self.bc_pos
-        vel[..., 0, :] = self.bc_vel
+        vel[..., 0, :] = self.bc_vel * self.phase_gn.tau[..., None]
 
         # Get scaled time increment steps
         scaled_times = LinearPhaseGenerator.phase(self.phase_gn, self.times)
@@ -143,6 +143,9 @@ class DMP(MPInterface):
                 pos[..., i, :] + torch.einsum('...,...i->...i',
                                               scaled_dt[..., i],
                                               vel[..., i + 1, :])
+
+        # Unscale velocity to original time space
+        vel = vel / self.phase_gn.tau[..., None, None]
 
         # Store pos and vel
         self.pos = pos

mp_pytorch/idmp.py
@@ -350,7 +350,7 @@ class IDMP(ProMP):
         vel_cov = vel_cov + torch.eye(vel_cov.shape[-1]) * reg_term_vel
 
         # Unscale velocity to original time scale space
-        self.vel_cov = vel_cov / self.phase_gn.tau[..., None]**2
+        self.vel_cov = vel_cov / self.phase_gn.tau[..., None, None] ** 2
 
         return self.vel_cov
 
@@ -439,8 +439,7 @@ class IDMP(ProMP):
         trajs = torch.Tensor(trajs)
 
         # Get boundary conditions
-        # dt = (times[..., 1] - times[..., 0])[..., None]
-        dt = self.basis_gn.scaled_dt
+        dt = self.basis_gn.scaled_dt * self.phase_gn.tau
         bc_time = times[..., 0]
         bc_pos = trajs[..., 0, :]
         bc_vel = torch.diff(trajs, dim=-2)[..., 0, :] / dt
@@ -538,15 +537,18 @@ class IDMP(ProMP):
         vel_basis_bc_multi_dofs = self.basis_gn.vel_basis_multi_dofs(
             self.bc_time[..., None], self.num_dof)
 
+        # Scale bc_vel
+        bc_vel = self.bc_vel * self.phase_gn.tau[..., None]
+
         # Compute position and velocity determined part (part 1 and 2)
         # Position and velocity part 1 and part 2
         # Einsum shape: [*add_dim, num_times],
         #               [*add_dim, num_dof]
         #            -> [*add_dim, num_dof, num_times]
         pos_det = torch.einsum('...j,...i->...ij', xi_1, self.bc_pos) \
-                  + torch.einsum('...j,...i->...ij', xi_2, self.bc_vel)
+                  + torch.einsum('...j,...i->...ij', xi_2, bc_vel)
         vel_det = torch.einsum('...j,...i->...ij', dxi_1, self.bc_pos) \
-                  + torch.einsum('...j,...i->...ij', dxi_2, self.bc_vel)
+                  + torch.einsum('...j,...i->...ij', dxi_2, bc_vel)
 
         # Reshape: [*add_dim, num_dof, num_times]
         #       -> [*add_dim, num_dof * num_times]