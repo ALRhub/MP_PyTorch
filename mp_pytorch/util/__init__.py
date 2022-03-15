e8a9bc2af23807978542cb2e3309364f83831a8f
ge.li <ge.li@kit.edu>
2022-03-15 16:43:04 +0100

    recover more util functions

--- mp_pytorch/util/__init__.py
+++ mp_pytorch/util/__init__.py
@@ -2,3 +2,4 @@ from .util_debug import *
 from .util_matrix import *
 from .util_string import *
 from .util_data_structure import *
+from .util_media import *
---
+++ mp_pytorch/util/util_media.py
@@ -0,0 +1,41 @@
+"""
+    Utilities for generating media stuff
+"""
+
+from typing import Union
+
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+
+import mp_pytorch.util as util
+
+
+def fill_between(x: Union[np.ndarray, torch.Tensor],
+                 y_mean: Union[np.ndarray, torch.Tensor],
+                 y_std: Union[np.ndarray, torch.Tensor],
+                 std_scale: int = 2, draw_mean: bool = False,
+                 alpha=0.2, color='gray'):
+    """
+    Utilities to draw std plot
+    Args:
+        x: x value
+        y_mean: y mean value
+        y_std: standard deviation of y
+        std_scale: filling range of [-scale * std, scale * std]
+        draw_mean: plot mean curve as well
+        alpha: transparency of std plot
+        color: color to fill
+
+    Returns:
+        None
+    """
+    x, y_mean, y_std = util.to_nps(x, y_mean, y_std)
+
+    axis = plt.gca()
+    if draw_mean:
+        axis.plot(x, y_mean, color=color)
+    axis.fill_between(x=x,
+                      y1=y_mean - std_scale * y_std,
+                      y2=y_mean + std_scale * y_std,
+                      alpha=alpha, color=color)
