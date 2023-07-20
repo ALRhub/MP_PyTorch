3da956d3c379524272106908b54df9f046a75610
ge.li <ge.li@kit.edu>
2023-07-20 16:35:05 +0200

    make the input to nn.param

--- test/learnable_tau.py
+++ test/learnable_tau.py
@@ -20,6 +20,27 @@ DEFAULT_VALUES = dict(
     ),
 )
 
+class SimplePredictor(torch.nn.Module):
+    def __init__(self, input_size: int, output_size: int, learn_tau: bool):
+        super().__init__()
+
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(input_size, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, output_size),
+            torch.nn.ReLU()
+        )
+
+        self.learn_tau = learn_tau
+        self.min_tau = 0.3
+        self.max_tau = 3.0
+
+    def forward(self, x):
+        output = self.net(x)
+        return output + 1
+
 
 def print_computational_graph(grad_fn, level: int = 0):
     for next_grad_fn, _ in grad_fn.next_functions:
@@ -51,53 +72,29 @@ def print_computational_graph(grad_fn, level: int = 0):
             print_computational_graph(next_grad_fn, level + 1)
 
 
-class SimplePredictor(torch.nn.Module):
-    def __init__(self, input_size: int, output_size: int, learn_tau: bool):
-        super().__init__()
-
-        self.net = torch.nn.Sequential(
-            torch.nn.Linear(input_size, 32),
-            torch.nn.ReLU(),
-            torch.nn.Linear(32, 32),
-            torch.nn.ReLU(),
-            torch.nn.Linear(32, output_size),
-            torch.nn.ReLU()
-        )
-
-        self.learn_tau = learn_tau
-        self.min_tau = 0.3
-        self.max_tau = 3.0
-
-    def forward(self, x):
-        output = self.net(x)
-        return output + 1
-
-
 if __name__ == "__main__":
     mp = MPFactory.init_mp(**DEFAULT_VALUES)
 
-    # Create a simple predictor
-    input_size = 10
-    output_size = DEFAULT_VALUES["num_dof"] * DEFAULT_VALUES["mp_args"][
-        "num_basis"] + DEFAULT_VALUES["num_dof"] + 1 * DEFAULT_VALUES[
-                      "learn_tau"]
+    batch_size = 1
     output_size = DEFAULT_VALUES["num_dof"] * DEFAULT_VALUES["mp_args"][
         "num_basis"] + 1 * DEFAULT_VALUES["learn_tau"]
 
+    # predictor = torch.abs((torch.randn((1, output_size)).to(DEFAULT_VALUES["device"])))
+    # predictor = torch.nn.Parameter(data=predictor)
+    # optimizer = torch.optim.Adam([predictor,], lr=1e-3)
+    #
+    input_size = 3
     predictor = SimplePredictor(input_size, output_size,
-                                DEFAULT_VALUES["learn_tau"]).to(
-        DEFAULT_VALUES["device"])
-
+                                DEFAULT_VALUES["learn_tau"]).to(DEFAULT_VALUES["device"])
     optimizer = torch.optim.Adam(predictor.parameters(), lr=1e-3)
 
-    batch_size = 80
+
+
 
     prediction_times = torch.arange(0, 1, DEFAULT_VALUES["mp_args"]["dt"]).to(
         DEFAULT_VALUES["device"]).unsqueeze(0).repeat(batch_size, 1)
 
-    for iteration in range(1):
-        # Generate random input
-        x = torch.randn(batch_size, input_size).to(DEFAULT_VALUES["device"])
+    for iteration in range(3):
 
         referenc_trajectory = torch.randn((batch_size,
                                            prediction_times.shape[-1],
@@ -112,8 +109,10 @@ if __name__ == "__main__":
             DEFAULT_VALUES["device"]).unsqueeze(0).repeat(batch_size, 1)
 
         # Predict the parameters of the MP
-        params = predictor(x)
-
+        if isinstance(predictor, torch.nn.Module):
+            params = predictor(torch.randn((batch_size, input_size)).to(DEFAULT_VALUES["device"]))
+        else:
+            params = predictor
         # Predict trajectory
         mp.update_inputs(
             times=prediction_times,
@@ -129,11 +128,9 @@ if __name__ == "__main__":
         # Compute the loss
         loss = trajectory.mean()
 
-        print_computational_graph(loss.grad_fn)
+        # print_computational_graph(loss.grad_fn)
 
         optimizer.zero_grad(set_to_none=True)
-
-        params = mp.get_params()
 
         loss.backward()
 
